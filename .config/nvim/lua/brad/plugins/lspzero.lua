800ec3f635ba820b9832e38325b4fc02f43d1c8f
Unappendixed <brad@bscarlson.com>
2024-04-06 10:08:34 -0700

    nvim: Remove unused CR mapping

--- .config/nvim/lua/brad/plugins/lspzero.lua
+++ .config/nvim/lua/brad/plugins/lspzero.lua
@@ -34,7 +34,6 @@ return {
                         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                        ['<CR>'] = cmp.mapping.confirm({ select = true }),
                         ['<C-Space>'] = cmp.mapping.complete(),
                     }),
                     snippet = {
