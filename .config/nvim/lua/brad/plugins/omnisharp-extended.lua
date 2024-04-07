cd86e78d7808ad55b9d7e59cfc88900710792c84
Unappendixed <brad@bscarlson.com>
2024-04-06 23:55:48 -0700

    nvim: fix omnisharp autocmd

--- .config/nvim/lua/brad/plugins/omnisharp-extended.lua
+++ .config/nvim/lua/brad/plugins/omnisharp-extended.lua
@@ -4,10 +4,16 @@ return {
         vim.api.nvim_create_autocmd("FileType", {
             pattern = "cs",
             callback = function(data)
-                print("called remap autocmd")
+                -- print("called remap autocmd")
                 local bufnr = data.buf
-                vim.keymap.set("n", "gd", function() require("omnisharp_extended").lsp_definition() end, {buffer = bufnr})
-                vim.keymap.set("n", "<leader>vrr", function() require("omnisharp_extended").lsp_references() end, {buffer = bufnr})
+
+                vim.api.nvim_create_autocmd("LspAttach", {
+                    buffer = bufnr,
+                    callback = function()
+                        vim.keymap.set("n", "gd", function() require("omnisharp_extended").lsp_definition() end, {buffer = bufnr})
+                        vim.keymap.set("n", "<leader>vrr", function() require("omnisharp_extended").lsp_references() end, {buffer = bufnr})
+                    end
+                })
             end
         })
     end
