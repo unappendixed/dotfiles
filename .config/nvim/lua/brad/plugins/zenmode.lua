580eb47c6fb5a5f403251290f3123601fa1c7d3d
Unappendixed <38593737+unappendixed@users.noreply.github.com>
2024-05-17 10:18:50 -0700

    nvim: Add height prop to zenmode config

--- .config/nvim/lua/brad/plugins/zenmode.lua
+++ .config/nvim/lua/brad/plugins/zenmode.lua
@@ -7,6 +7,7 @@ return {
                     number = true,
                 },
                 width = 100,
+                height = 90,
             },
             plugins = {
                 options = {
