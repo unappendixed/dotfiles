2e9897a6586919da88bcd6d71b5f267c446cf661
Unappendixed <38593737+unappendixed@users.noreply.github.com>
2024-06-24 10:47:26 -0700

    nvim: bunch o' changes

--- .config/nvim/lua/brad/plugins/dap.lua
+++ .config/nvim/lua/brad/plugins/dap.lua
@@ -79,6 +79,25 @@ return {
                     -- ignored by delve in dap mode.
                     build_flags = "",
                 },
+
+            })
+
+            local function get_arguments()
+                return coroutine.create(function(dap_run_co)
+                    local args = {}
+                    vim.ui.input({ prompt = "Args: " }, function(input)
+                        args = vim.split(input or "", " ")
+                        coroutine.resume(dap_run_co, args)
+                    end)
+                end)
+            end
+
+            table.insert(require("dap").configurations.go, 4, {
+                type = "go",
+                name = "Debug Package (Arguments)",
+                request = "launch",
+                program = "${fileDirname}",
+                args = get_arguments,
             })
         end,
     },
